bfe2da7b07005b92d1917b2fa17be242f95a004e
salvador <salgonzolanda@gmail.com>
2019-10-17 16:06:28 -0700

    mvp met

---
+++ data/seeds/01-employees.js
@@ -0,0 +1,12 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {username: 'Sadie', password: 'password1', department: 'management'},
+        {username: 'Ben', password: 'password2', department: 'accounting'},
+        {username: 'Alice', password: 'password3', department: 'devOps'}
+      ]);
+    });
+};
--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,24 @@
 // Update with your config settings.
-
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/users.db3'
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
   },
-
+  migrations: {
+    directory: './data/migrations',
+  },
+  seeds: {
+    directory: './data/seeds',
+  },
+},
   staging: {
     client: 'postgresql',
     connection: {
